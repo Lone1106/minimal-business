65d498504b29d458f82a9eea4029ffb6ff4d4d1a
Jan Reichherzer <jan.reichherzer@gmx.de>
2023-05-24 17:33:31 +0200

    Added basic shopping cart html and styling as well as basic js for opening and closing. Will add shopping cart logic next

--- src/script.js
+++ src/script.js
@@ -4,10 +4,18 @@ const mobileNav = document.querySelector(".mobile-nav");
 const navItems = document.querySelectorAll(".nav-item");
 const navItemsMobile = document.querySelectorAll(".nav-item-mo");
 const copy = document.querySelector(".copy");
+const cart = document.querySelector(".cart");
+const cartOpen = document.querySelector(".cart-open");
+const cartClose = document.querySelector(".cart-close");
+const buyNow = document.querySelector(".buy-now");
+const cartList = document.querySelector(".cart-list");
+const cartTotal = document.querySelector(".cart-total");
 
+// COPY YEAR FOOTER
 let year = new Date().getFullYear();
 copy.innerHTML = year;
 
+// MOBILE NAVIGATION OPEN - CLOSE - ACTIVE CLASSES
 mobileNavOpen.addEventListener("click", () => {
     mobileNav.classList.add("menu-open");
 });
@@ -35,3 +43,16 @@ navItemsMobile.forEach((i) => {
         i.classList.add("active-nav");
     });
 });
+
+// SHOPPING CART OPEN - CLOSE
+cartOpen.addEventListener("click", () => {
+    cart.classList.add("menu-open");
+});
+
+cartClose.addEventListener("click", () => {
+    cart.classList.remove("menu-open");
+});
+
+// SHOPPING CART BEHAVIOUR
+const products = [];
+const shoppingCartItems = [];
